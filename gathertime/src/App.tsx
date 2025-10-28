7c6455fdf754e0086549b4fb32712246a293debc
Dominykas Stunžėnas <dominykas.stunzenas@gmail.com>
2025-10-28 22:49:39 +0200

    DP-102: added UI component and logic for login and navigation to calendar page

--- gathertime/src/App.tsx
+++ gathertime/src/App.tsx
@@ -2,15 +2,18 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AppRoutes } from './utilities/Routes'
 import Registration from './components/authentication/Registration'
+import Login from './components/authentication/Login'
+import Calendar from './components/main/Calendar'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Navigate to={AppRoutes.CALENDAR} />} />
-        
+        <Route index element={<Navigate to={AppRoutes.LOG_IN} />} />
+        <Route path={AppRoutes.LOG_IN} element={<Login />} />
         <Route path={AppRoutes.SIGN_UP} element={<Registration />} />
+        <Route path={AppRoutes.CALENDAR} element={<Calendar />} />
       </Routes>
     </BrowserRouter>
   )
